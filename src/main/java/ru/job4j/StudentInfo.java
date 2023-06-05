bc3f2919dff681929220c2d43c93a39c63484ad6
Альберт <albert1702@ya.ru>
2023-06-05 18:46:05 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[]args) {
+        System.out.println("Альберт Исманский");
+        System.out.println("30.05.1985");
+    }
+}
