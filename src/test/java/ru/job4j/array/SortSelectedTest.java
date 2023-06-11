e781d0c20aceeba2d7b7d7bf01469dbdc80e0250
Альберт <albert1702@ya.ru>
2023-06-11 13:05:27 +0300

    6.5.3. Сортировка выборкой. [#156320  [#156320 #422409]]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -11,4 +11,20 @@ public class SortSelectedTest {
         int[] expected = new int[] {1, 2, 3, 4, 5};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    public void whenSortTwy() {
+        int[] data = new int[] {10, 45, 89, 73, -5, -54};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {-54, -5, 10, 45, 73, 89};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortFree() {
+        int[] data = new int[] {150, 345, -989, -673, -45, -954};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {-989, -954, -673, -45, 150, 345};
+        assertThat(result).containsExactly(expected);
+    }
 }
